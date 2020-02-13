5545b5d6edad8087e14d6518f6ca1d1d6e7a035f
Ryan Stoughton <ryandstoughton@gmail.com>
2020-02-12 21:18:10 -0600

    First commit

---
+++ src/main/java/com/stoughton/ryan/aggregit/AggregitApplication.java
@@ -0,0 +1,13 @@
+package com.stoughton.ryan.aggregit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stoughton/ryan/aggregit/AggregitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stoughton.ryan.aggregit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
