8382a5222ebddabdebb8fa14bd73d3526a01dc24
Ryan Stoughton <ryandstoughton@gmail.com>
2020-02-16 14:07:33 -0600

    Add git client

--- src/main/java/com/stoughton/ryan/aggregit/github/GithubClient.java
+++ src/main/java/com/stoughton/ryan/aggregit/github/GithubClient.java
@@ -1,14 +1,44 @@
 package com.stoughton.ryan.aggregit.github;
 
+import com.stoughton.ryan.aggregit.domain.GithubContributions;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class GithubClient {
+
   private WebClient webClient;
 
-  public GithubClient(final WebClient webClient) {
+  public GithubClient(WebClient webClient) {
     this.webClient = webClient;
   }
 
+  public Mono<GithubContributions> getUserContributions(String username) {
+    return webClient.post()
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(BodyInserters.fromValue(buildUserContributionsQuery(username)))
+        .retrieve()
+        .bodyToMono(GithubContributions.class);
+  }
+
+  private String buildUserContributionsQuery(String username) {
+    return "\"query\": \"query {\n"
+        + "  user(login: \"" + username + "\") {\n"
+        + "    login\n"
+        + "    contributionsCollection {\n"
+        + "      contributionCalendar {\n"
+        + "        weeks {\n"
+        + "          contributionDays {\n"
+        + "            contributionCount\n"
+        + "            weekday\n"
+        + "          }\n"
+        + "        }\n"
+        + "      }\n"
+        + "    }\n"
+        + "  }\n"
+        + "}\n\"";
+  }
 }
--- src/test/java/com/stoughton/ryan/aggregit/github/GithubClientIntegrationTest.java
+++ src/test/java/com/stoughton/ryan/aggregit/github/GithubClientIntegrationTest.java
@@ -1,43 +1,78 @@
 package com.stoughton.ryan.aggregit.github;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stoughton.ryan.aggregit.domain.GithubContributions;
 import com.stoughton.ryan.aggregit.domain.GithubContributions.ContributionsData;
-import com.stoughton.ryan.aggregit.domain.GithubContributions.ContributionsData.Viewer;
+import com.stoughton.ryan.aggregit.domain.GithubContributions.ContributionsData.User;
 import java.io.IOException;
-import okhttp3.HttpUrl;
+import java.util.function.Consumer;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.test.StepVerifier;
 
-class GithubClientTest {
+/*
+ * Heavily references spring-framework's WebClientIntegrationTests.java for test configuration
+ */
+class GithubClientIntegrationTest {
 
-  ObjectMapper mapper = new ObjectMapper();
+  private ObjectMapper mapper;
 
-  WebClient client;
-  GithubClient subject;
+  private MockWebServer server;
+
+  private WebClient webClient;
+
+  private GithubClient subject;
+
+  @BeforeEach
+  void setUp() {
+    mapper = new ObjectMapper();
+  }
+
+  @AfterEach
+  void tearDown() throws IOException {
+    this.server.shutdown();
+  }
 
   @Test
   void getUserContributions() throws IOException {
-    MockWebServer githubServer = new MockWebServer();
-    githubServer.enqueue(new MockResponse().setBody(
-        mapper.writeValueAsString(
-            GithubContributions.builder()
-                .data(ContributionsData.builder()
-                    .viewer(Viewer.builder()
-                        .login("someuser")
-                        .build())
-                    .build()))));
+    startServer();
 
-    githubServer.start();
-    HttpUrl url = githubServer.url("");
-
-    client = WebClient.builder()
-        .baseUrl(url.toString())
+    GithubContributions expectedBody = GithubContributions.builder()
+        .data(ContributionsData.builder()
+            .user(User.builder()
+                .login("someuser")
+                .build())
+            .build())
         .build();
-    subject = new GithubClient(client);
+    String expectedBodyJson = mapper.writeValueAsString(expectedBody);
+
+    prepareResponse(response -> response
+        .setHeader("content-type", "application/json")
+        .setBody(expectedBodyJson));
+
+    StepVerifier.create(subject.getUserContributions("someuser"))
+        .consumeNextWith(response -> assertThat(response).isEqualTo(expectedBody))
+        .expectComplete()
+        .verify();
+  }
+
+  private void startServer() {
+    this.server = new MockWebServer();
+    this.webClient = WebClient.builder()
+        .baseUrl(this.server.url("/").toString())
+        .build();
+    this.subject = new GithubClient(webClient);
+  }
+
+  private void prepareResponse(Consumer<MockResponse> consumer) {
+    MockResponse response = new MockResponse();
+    consumer.accept(response);
+    this.server.enqueue(response);
   }
 }
