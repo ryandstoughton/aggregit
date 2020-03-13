3ad124ec8c7101e2fc135bcb1f837314c2e3a6f1
Ryan Stoughton <ryandstoughton@gmail.com>
2020-03-13 16:11:48 -0500

    Trim Github days down to 366 to match Gitlab

--- src/main/java/com/stoughton/ryan/aggregit/github/GithubService.java
+++ src/main/java/com/stoughton/ryan/aggregit/github/GithubService.java
@@ -5,11 +5,15 @@ import com.stoughton.ryan.aggregit.github.GithubContributions.ContributionsData.
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Service
 public class GithubService {
+  private final int GIT_CONTRIBUTION_DAYS_IN_A_YEAR = 366;
 
   private GithubClient githubClient;
 
@@ -28,14 +32,29 @@ public class GithubService {
           .getContributionCalendar()
           .getWeeks();
 
-      return Mono.just(contributionWeeks.stream()
-          .flatMap(week -> week.getContributionDays()
-              .stream()
-              .map(day -> GitContributionDay.builder()
-                  .count(day.getContributionCount())
-                  .dayOfYear(dayOfYear.getAndIncrement())
-                  .build()))
+      Mono<List<GitContributionDay>> days = Mono.just(contributionWeeks.stream()
+          .flatMap(week -> {
+            Stream<GitContributionDay> gitContributionDayStream = week.getContributionDays()
+                .stream()
+                .map(day -> GitContributionDay.builder()
+                    .count(day.getContributionCount())
+                    .dayOfYear(dayOfYear.getAndIncrement())
+                    .build());
+            log.info(">> days: {}", gitContributionDayStream);
+            return gitContributionDayStream;
+          })
           .collect(Collectors.toList()));
+
+      if (dayOfYear.get() > GIT_CONTRIBUTION_DAYS_IN_A_YEAR) { // Too many days mapped
+        days = days.flatMap(gitContributionDays -> {
+          AtomicInteger numDays = new AtomicInteger(gitContributionDays.size());
+          AtomicInteger newDayOfYear = new AtomicInteger(0);
+          gitContributionDays.removeIf(ignored -> numDays.getAndDecrement() > GIT_CONTRIBUTION_DAYS_IN_A_YEAR);
+          gitContributionDays.forEach(day -> day.setDayOfYear(newDayOfYear.getAndIncrement()));
+          return Mono.just(gitContributionDays);
+        });
+      }
+      return days;
     });
   }
 }
--- src/test/java/com/stoughton/ryan/aggregit/github/GithubServiceTest.java
+++ src/test/java/com/stoughton/ryan/aggregit/github/GithubServiceTest.java
@@ -1,10 +1,12 @@
 package com.stoughton.ryan.aggregit.github;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
 import com.stoughton.ryan.aggregit.git.GitContributionDay;
 import java.io.IOException;
 import java.util.List;
@@ -44,9 +46,16 @@ class GithubServiceTest {
         new ClassPathResource("gitContributionsForGithubConversion.json").getInputStream(),
         "UTF-8");
 
-    List<GitContributionDay> expectedGitContributionDays = mapper
+    List<GitContributionDay> expectedGitContributionDays = Lists.newArrayList();
+    for (int i = 0; i < 352; i++) {
+      expectedGitContributionDays.add(GitContributionDay.builder()
+          .dayOfYear(i)
+          .count(0)
+          .build());
+    }
+    expectedGitContributionDays.addAll(mapper
         .readValue(gitContributionsJson, new TypeReference<>() {
-        });
+        }));
 
     githubContributionsJson = IOUtils.inputStreamAsString(
         new ClassPathResource("githubContributionsForGitConversion.json").getInputStream(),
@@ -60,6 +69,47 @@ class GithubServiceTest {
         .as("Call to subject that should produce some expected GitContributionsDays")
         .assertNext(gitContributions ->
             Assertions.assertEquals(gitContributions, expectedGitContributionDays))
+        .expectComplete()
+        .verify();
+
+    verify(githubClient).userContributions(username);
+  }
+
+  @Test
+  void userContributions_ignoresDaysOneYearOrMoreAgo() throws IOException {
+    String username = "someuser";
+
+    gitContributionsJson = IOUtils.inputStreamAsString(
+        new ClassPathResource("gitContributionsForGithubConversion.json")
+            .getInputStream(),
+        "UTF-8");
+
+    // Create expected list
+    List<GitContributionDay> expectedGitContributionDays = Lists.newArrayList();
+    for (int i = 0; i < 352; i++) {
+      expectedGitContributionDays.add(GitContributionDay.builder()
+          .dayOfYear(i)
+          .count(0)
+          .build());
+    }
+    expectedGitContributionDays.addAll(mapper
+        .readValue(gitContributionsJson, new TypeReference<>() {
+        }));
+    assertThat(expectedGitContributionDays.size()).isEqualTo(366);
+
+
+    githubContributionsJson = IOUtils.inputStreamAsString(
+        new ClassPathResource("githubContributionsForGitConversionWithOldContributions.json")
+            .getInputStream(),
+        "UTF-8");
+    GithubContributions githubContributions = mapper
+        .readValue(githubContributionsJson, GithubContributions.class);
+    when(githubClient.userContributions(username))
+        .thenReturn(Mono.just(githubContributions));
+
+    StepVerifier.create(subject.userContributions(username))
+        .assertNext(gitContributions ->
+            Assertions.assertEquals(expectedGitContributionDays, gitContributions))
         .expectComplete()
         .verify();
 
